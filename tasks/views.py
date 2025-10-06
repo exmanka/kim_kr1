309c99f7b1cf59433dcb6ef10f60359975a51acb
Michael Kim <74555362+exmanka@users.noreply.github.com>
2025-10-06 18:41:38 +0000

    1_Kim

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,16 @@
 import os
 from django.shortcuts import render
 
+
 def index(request):
+  # Прочитать определенную строку из файла
+  with open('tasks/templates/kim.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    # Например, взять 3-ю строку (индекс 2)
+    text_from_file = lines[5].strip() if len(lines) > 2 else ''
+
   context = {
-    'some_exposed_secret': os.environ.get('FIREBASE_API_KEY')
+      'some_exposed_secret': os.environ.get('FIREBASE_API_KEY'),
+      'text_from_file': text_from_file
   }
   return render(request, "index.html", context)
