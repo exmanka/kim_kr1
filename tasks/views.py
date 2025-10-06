bb4ead0f058c16e2a62c79e8275f52ae63bd34eb
Michael Kim <74555362+exmanka@users.noreply.github.com>
2025-10-06 19:00:20 +0000

    2_Kim

--- tasks/views.py
+++ tasks/views.py
@@ -8,9 +8,11 @@ def index(request):
     lines = f.readlines()
     # Например, взять 3-ю строку (индекс 2)
     text_from_file = lines[5].strip() if len(lines) > 2 else ''
+    text_from_file2 = lines[435].strip() if len(lines) > 2 else ''
 
   context = {
       'some_exposed_secret': os.environ.get('FIREBASE_API_KEY'),
-      'text_from_file': text_from_file
+      'text_from_file': text_from_file,
+      'text_from_file2': text_from_file2,
   }
   return render(request, "index.html", context)
